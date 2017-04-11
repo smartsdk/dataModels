026d3dcebbe15c28313f0634d9bbab6fe8e8046d
Federico M. Facca <federico.facca@gmail.com>
2017-04-11 14:43:39 +0200

    fixed ci shunit2 test for WeatherObserved

--- test/ci-test-weather.sh
+++ test/ci-test-weather.sh
@@ -3,10 +3,10 @@
 # Tests the Weather datamodels
 
 testWeatherObserved(){
-   result = ajv compile --v5 -s ../Weather/WeatherObserved/schema.json -r ../common-schema.json -r ../geometry-schema.json -r ../Weather/weather-schema.json
-   assertEquals "../Weather/WeatherObserved/example.json valid" "${result}"
-   result = ajv test --v5 -s ../Weather/WeatherObserved/schema.json -r ../common-schema.json -r ../geometry-schema.json -r ../Weather/weather-schema.json -d ../Weather/WeatherObserved/example.json --valid
-   assertEquals "schema ../Weather/WeatherObserved/schema.json is valid" "${result}"
+   result=`ajv compile --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json`
+   assertEquals "schema Weather/WeatherObserved/schema.json is valid" "${result}"
+   result=`ajv test --v5 -s Weather/WeatherObserved/schema.json -r common-schema.json -r geometry-schema.json -r Weather/weather-schema.json -d Weather/WeatherObserved/example.json --valid`
+   assertEquals "Weather/WeatherObserved/example.json passed test" "${result}"
 }
 
 # load shunit2
